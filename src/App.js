e49990a311e06d3a6df99c243d573c1e5ecf4799
ciloemre <emrecilo53@hotmail.com>
2024-07-21 01:07:32 +0300

    Added Features

--- src/App.js
+++ src/App.js
@@ -168,166 +168,33 @@ function App() {
         </ul>
       </div>
       {/* Heart and Arrow Animation */}
-      <div
-        style={{
-          position: "absolute",
-          top: "651px",
-          left: "-39px",
-          zIndex: 10,
-          width: "150px",
-          height: "150px",
-        }}
-      >
+      <div className="heart-container heart-left">
         <motion.div
-          animate={{ rotate: [45] }}
+          animate={{ rotate: [135] }}
           transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            position: "relative",
-            fontSize: "40px",
-            color: "red",
-          }}
+          className="heart-animation"
         >
-          <div
-            style={{
-              position: "relative",
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faHeart}
-              style={{ fontSize: "100px", color: "red" }}
-            />
-            <div
-              style={{
-                position: "absolute",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "100%",
-                top: "0",
-                left: "0",
-              }}
-            >
-              <span
-                style={{
-                  position: "absolute",
-                  fontSize: "30px",
-                  fontWeight: "bold",
-                  color: "white",
-                  top: "20%",
-                  left: "27%",
-                }}
-              >
-                E
-              </span>
-              <span
-                style={{
-                  position: "absolute",
-                  fontSize: "30px",
-                  fontWeight: "bold",
-                  color: "white",
-                  top: "20%",
-                  left: "60%",
-                }}
-              >
-                Z
-              </span>
+          <div className="heart-content">
+            <FontAwesomeIcon icon={faHeart} className="heart-icon" />
+            <div className="heart-labels">
+              <span className="label-e">E</span>
+              <span className="label-z">Z</span>
             </div>
           </div>
         </motion.div>
       </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "507px",
-          right: "45px",
-          zIndex: 10,
-          width: "150px",
-          height: "150px",
-        }}
-      >
+      <div className="heart-container heart-right">
         <motion.div
           animate={{ rotate: [0, 360] }}
           transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            position: "relative",
-            fontSize: "40px",
-            color: "red",
-          }}
+          className="heart-animation"
         >
-          <div
-            style={{
-              position: "relative",
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faHeart}
-              style={{ fontSize: "220px", color: "red" }}
-            />
-            <div
-              style={{
-                position: "absolute",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "100%",
-                top: "0",
-                left: "0",
-              }}
-            >
-              <span
-                style={{
-                  position: "absolute",
-                  fontSize: "32px",
-                  fontWeight: "bold",
-                  color: "white",
-                  top: "19%",
-                  left: "13%",
-                }}
-              >
-                Sınırsız
-              </span>
-              <span
-                style={{
-                  position: "absolute",
-                  fontSize: "32px",
-                  fontWeight: "bold",
-                  color: "white",
-                  top: "36%",
-                  left: "38%",
-                }}
-              >
-                ve
-              </span>
-              <span
-                style={{
-                  position: "absolute",
-                  fontSize: "32px",
-                  fontWeight: "bold",
-                  color: "white",
-                  top: "51%",
-                  left: "13%",
-                }}
-              >
-                Sonsuz...
-              </span>
+          <div className="heart-content">
+            <FontAwesomeIcon icon={faHeart} className="heart-icon-large" />
+            <div className="heart-labels-large">
+              <span className="label-limitless">Sınırsız</span>
+              <span className="label-and">ve</span>
+              <span className="label-infinite">Sonsuz...</span>
             </div>
           </div>
         </motion.div>
