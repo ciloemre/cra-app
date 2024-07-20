9ee218a7c04d4f2384dc839a1ca6b9b1b1ef2654
ciloemre <emrecilo53@hotmail.com>
2024-07-20 23:28:22 +0300

    Addet Features

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import {
   faEdit,
   faSave,
   faCheck,
+  faHeart,
 } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
 
 function App() {
   const [text, setText] = useState("");
@@ -80,7 +82,7 @@ function App() {
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center", position: "relative" }}>
       <table
         style={{
           padding: "10px",
@@ -95,6 +97,7 @@ function App() {
           height: "auto",
           margin: "10px",
           border: "2px solid black",
+          position: "relative",
         }}
       >
         <p
@@ -222,6 +225,148 @@ function App() {
           ))}
         </ul>
       </table>
+
+      {/* Heart and Arrow Animation */}
+      <div
+        style={{
+          position: "absolute",
+          top: "640px",
+          left: "-46px",
+          zIndex: 10,
+          width: "150px",
+          height: "150px",
+        }}
+      >
+        <motion.div
+          animate={{ rotate: [45] }}
+          transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            position: "relative",
+            fontSize: "40px",
+            color: "red",
+          }}
+        >
+          <div
+            style={{
+              position: "relative",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faHeart}
+              style={{ fontSize: "100px", color: "red" }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "100%",
+                top: "0",
+                left: "0",
+              }}
+            >
+              <span
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  fontWeight: "bold",
+                  color: "white",
+                  top: "20%",
+                  left: "27%",
+                }}
+              >
+                E
+              </span>
+              <span
+                style={{
+                  position: "absolute",
+                  fontSize: "30px",
+                  fontWeight: "bold",
+                  color: "white",
+                  top: "20%",
+                  left: "60%",
+                }}
+              >
+                Z
+              </span>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "490px",
+          right: "40px",
+          zIndex: 10,
+          width: "150px",
+          height: "150px",
+        }}
+      >
+        <motion.div
+          animate={{ rotate: [0, 360] }}
+          transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            position: "relative",
+            fontSize: "40px",
+            color: "red",
+          }}
+        >
+          <div
+            style={{
+              position: "relative",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faHeart}
+              style={{ fontSize: "220px", color: "red" }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "100%",
+                top: "0",
+                left: "0",
+              }}
+            >
+              <span
+                style={{
+                  position: "absolute",
+                  fontSize: "32px",
+                  fontWeight: "bold",
+                  color: "white",
+                  top: "18%",
+                  left: "6%",
+                }}
+              >
+                Sınırsız ve Sonsuz...
+              </span>
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 }
